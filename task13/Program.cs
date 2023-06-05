25e8317fc3bfd21825a04553848ed71a112938f4
Vatutina Maria <Vatmash03@mail.ru>
2023-06-05 20:17:48 +0500

    Второе задание

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int rand = new Random().Next();
+Console.WriteLine("Рандомное число: " + rand);
+
+FindLergestNumbers(rand);
+
+void FindLergestNumbers(int a)
+{
+    if (a < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine("Третья цифра в числе: " + a.ToString()[2]);
+    }
+}
